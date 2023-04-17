721d630c9c1b5b3be378ac7bf67a913038d4e315
CJQ-CS-FD <jiaqichen.cs@outlook.com>
2023-04-17 10:28:27 +0000

    UPDATE

--- scripts/main_ssa_engine.py
+++ scripts/main_ssa_engine.py
@@ -18,6 +18,8 @@ def parse_args():
     parser.add_argument('--out_dir', help='the dir to save semantic annotations')
     parser.add_argument('--save_img', default=False, action='store_true', help='whether to save annotated images')
     parser.add_argument('--world_size', type=int, default=0, help='number of nodes')
+    parser.add_argument('--sam', default=False, action='store_true', help='use SAM but not given annotation json, default is False')
+    parser.add_argument('--ckpt_path', help='specify the root path of SAM checkpoint')
     args = parser.parse_args()
     return args
     
@@ -39,7 +41,21 @@ def main(rank, args):
     clipseg_processor = AutoProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
     clipseg_model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined").to(rank)
     clipseg_processor.image_processor.do_resize = False
-    
+    if args.sam:
+        from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+        sam = sam_model_registry["vit_h"](checkpoint=args.ckpt_path).to(rank)
+        mask_generator = SamAutomaticMaskGenerator(
+            model=sam,
+            points_per_side=32,
+            pred_iou_thresh=0.86,
+            stability_score_thresh=0.92,
+            crop_n_layers=0,  # 1 by default
+            crop_n_points_downscale_factor=2,
+            min_mask_region_area=100,  # Requires open-cv to run post-processing
+            output_mode='coco_rle',
+        )
+    else:
+        mask_generator = None
     filenames = [fn_[:-5] for fn_ in os.listdir(args.data_dir) if '.json' in fn_]
     local_filenames = filenames[(len(filenames) // args.world_size + 1) * rank : (len(filenames) // args.world_size + 1) * (rank + 1)]
     for file_name in local_filenames:
@@ -48,7 +64,7 @@ def main(rank, args):
                                     oneformer_ade20k_processor=oneformer_ade20k_processor, oneformer_ade20k_model=oneformer_ade20k_model,
                                     oneformer_coco_processor=oneformer_coco_processor, oneformer_coco_model=oneformer_coco_model,
                                     blip_processor=blip_processor, blip_model=blip_model,
-                                    clipseg_processor=clipseg_processor, clipseg_model=clipseg_model)
+                                    clipseg_processor=clipseg_processor, clipseg_model=clipseg_model, mask_generator=mask_generator)
 
 if __name__ == '__main__':
     args = parse_args()
